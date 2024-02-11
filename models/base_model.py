b70cae33f4d6f056e00c845dcbb4ad32985c9252
olvvvadamilola <destric3@gmail.com>
2024-02-11 19:01:11 +0100

    update base model.

--- models/base_model.py
+++ models/base_model.py
@@ -22,10 +22,10 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
 
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the BaseModel class"""
