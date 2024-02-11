17cd502fb4b8a27aa1b933d7881bec9cb0852905
olvvvadamilola <destric3@gmail.com>
2024-02-11 18:29:13 +0100

    base model creates, first unit test

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Base model"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Base class for all models
+    """
+    def __init__(self, *args, **kwargs):
+        """Initialize Basemodel instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__,
+                                         self.id, self.__dict__)
+
+    def save(self):
+        """updates updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary repr. of a BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict.update({"__class__": self.__class__.__name__})
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
+        return obj_dict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""unit test for the base model class"""
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        """Test initialization of BaseModel instance"""
+        obj = BaseModel()
+        self.assertIsNotNone(obj.id)
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
+
+    def test_str(self):
+        """Test string representation of BaseModel instance"""
+        obj = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(str(obj), expected_str)
+
+    def test_save(self):
+        """Test save method of BaseModel instance"""
+        obj = BaseModel()
+        old_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(obj.updated_at, old_updated_at)
+
+    def test_to_dict(self):
+        """Test to_dict method of BaseModel instance"""
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+
+if __name__ == '__main__':
+    unittest.main()
